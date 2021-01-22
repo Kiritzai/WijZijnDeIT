8131be5e2b61e4ab4b1f8dd4db96b6e969a33492
Kiritzai <toonen.remy@gmail.com>
2021-01-22 15:51:25 +0100

    j

--- Scripts/Powershell/ActiveDirectoryTestCredentials.ps1
+++ Scripts/Powershell/ActiveDirectoryTestCredentials.ps1
@@ -1,16 +1,15 @@
 
 Clear-Host
 
+Write-Host ""
+Write-Host `t"Active Directory Test Credentials"
+Write-Host ""
+
 $userName = Read-Host "Username"
 $passWord = Read-Host "Password" | ConvertTo-SecureString -asPlainText -Force
 
 $credential = New-Object System.Management.Automation.PSCredential($userName, $passWord)
 $cred = Get-Credential -Credential $credential
-
-#Exit
-#$cred = Get-Credential | Out-Null
-#$username = $cred.username
-#$password = $cred.GetNetworkCredential().password
 
 # Get current domain using logged-on user's credentials
 $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
@@ -18,11 +17,11 @@ $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$cre
 
 if ([string]::IsNullOrWhitespace($domain.name))
 {
-    write-host "Authentication failed - please verify your username and password."
-    Read-Host "Press ENTER to continue..."
+    write-host "Authentication failed" -ForegroundColor Red
+    Read-Host ""
 }
 else
 {
-    write-host "Successfully authenticated with domain $domain.name"
-    Read-Host "Press ENTER to continue..."
+    write-host "Successfully authenticated with domain $domain.name" -ForegroundColor Green
+    Read-Host ""
 }
