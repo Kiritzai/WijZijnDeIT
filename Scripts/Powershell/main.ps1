86587ba20a4182cfec2c84ca40e761b54bf17468
Kiritzai <toonen.remy@gmail.com>
2021-01-22 16:31:54 +0100

    wwwww

--- Scripts/Powershell/main.ps1
+++ Scripts/Powershell/main.ps1
@@ -13,7 +13,7 @@ function Show-Menu
     Write-Host ""
     Write-Host `t"================ $Title ================"
     Write-Host `t"Press '1' for Testing AD Credentials."
-    Write-Host `t"Press '2' for this option."
+    Write-Host `t"Press '2' for Cleaning Windows Firewall Rules for RDS Servers"
     Write-Host `t"Press '3' for this option."
     Write-Host `t"Press 'q' to quit."
 }
@@ -25,7 +25,7 @@ do
     switch ($selection)
     {
         '1' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/ActiveDirectoryTestCredentials.ps1')) }
-        '2' { Write-Host 'You chose option #2' }
+        '2' { Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/FirewallClean.ps1')) }
         '3' { Write-Host 'You chose option #3' }
         #'q' { return }
     }
