5b99c10cf5078366463b51424bce6f15175e5796
Kiritzai <toonen.remy@gmail.com>
2021-01-28 10:44:28 +0100

    Test 4

--- Scripts/Powershell/test.ps1
+++ Scripts/Powershell/test.ps1
@@ -1,11 +1,11 @@
-# [System.Net.Cache.RequestCacheLevel]::NoCacheNoStore; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::SecurityProtocol -bor 3072; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/test.ps1' -Headers @{"Cache-Control"="no-cache"}))) -Silent:$true
+# [System.Net.Cache.RequestCacheLevel]::NoCacheNoStore; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::SecurityProtocol -bor 3072; &([scriptblock]::Create((Invoke-WebRequest -useb 'https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/test.ps1' -DisableKeepAlive -Headers @{'Cache-Control' = 'no-cache'}))) -Silent:$true
 
 param (
     [switch]$Silent = $false
 )
 
 if ($Silent) {
-    Write-Host "Test 3"
+    Write-Host "Test 4"
     Write-Host "Silent run"
 } else {
     Write-Host "Not Silent Run"
