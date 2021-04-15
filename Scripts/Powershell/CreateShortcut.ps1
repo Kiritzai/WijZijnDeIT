76d8fe5b5018d13e0cf5bd0cef3e4a33980e9bb1
Kiritzai <toonen.remy@gmail.com>
2021-04-15 16:17:34 +0200

    Text change

--- Scripts/Powershell/CreateShortcut.ps1
+++ Scripts/Powershell/CreateShortcut.ps1
@@ -9,12 +9,12 @@ if (Test-Path -Path $DesktopPath -PathType Leaf) {
     Write-Host "File [$($DesktopPath)] already exists"
 }
 
-Write-Host "Creating file [$($DesktopPath)]"
-
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($DesktopPath)
 $Shortcut.TargetPath = "powershell.exe"
 $Shortcut.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/main.ps1'))"
 $Shortcut.Save()
+
+Write-Host "Created file [$($DesktopPath)]"
 
 $UserInput = $Host.UI.ReadLine()
