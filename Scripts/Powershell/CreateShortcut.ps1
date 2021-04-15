178d663574527fcd8e165351cab72b400c42c73d
Kiritzai <toonen.remy@gmail.com>
2021-04-15 16:22:24 +0200

    Created file issue

--- Scripts/Powershell/CreateShortcut.ps1
+++ Scripts/Powershell/CreateShortcut.ps1
@@ -7,14 +7,16 @@ $DesktopPath = Join-Path "$DesktopPath" "WijZijnDeIT.lnk"
 
 if (Test-Path -Path $DesktopPath -PathType Leaf) {
     Write-Host "File [$($DesktopPath)] already exists"
+} else {
+    $WshShell = New-Object -comObject WScript.Shell
+    $Shortcut = $WshShell.CreateShortcut($DesktopPath)
+    $Shortcut.TargetPath = "powershell.exe"
+    $Shortcut.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/main.ps1'))"
+    $Shortcut.Save()
+    
+    Write-Host "Created file [$($DesktopPath)]"
 }
 
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut($DesktopPath)
-$Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-NoProfile -ExecutionPolicy Bypass -Command `"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/main.ps1'))"
-$Shortcut.Save()
 
-Write-Host "Created file [$($DesktopPath)]"
 
 $UserInput = $Host.UI.ReadLine()
