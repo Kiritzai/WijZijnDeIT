7a04caea81d6ede18997644d2cab95f3525f867f
Kiritzai <toonen.remy@gmail.com>
2021-04-15 16:24:28 +0200

    Importing module

--- Scripts/Powershell/ActiveDirectoryComputerList.ps1
+++ Scripts/Powershell/ActiveDirectoryComputerList.ps1
@@ -14,6 +14,7 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 if ((Get-CimInstance -ClassName CIM_OperatingSystem).Caption -match 'Windows 10') {
     Add-WindowsCapability –online –Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
 } else {
+    Import-Module ServerManager
     $rsatAD = Get-WindowsFeature | Where-Object {$_.Name -eq "RSAT-AD-PowerShell"}
     if($rsatAD.Installed -eq "False") { Install-Feature -Name RSAT-AD-PowerShell }
 }
