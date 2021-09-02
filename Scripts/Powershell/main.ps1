8e73990cc8c812c002e296c567654bd64888fd35
Kiritzai <toonen.remy@gmail.com>
2021-09-02 16:08:20 +0200

    Update main.ps1

--- Scripts/Powershell/main.ps1
+++ Scripts/Powershell/main.ps1
@@ -13,8 +13,9 @@
 
 # Check for administrator rights
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] “Administrator”)) {
-    Write-Warning “You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!”
-    Break
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    $UserInput = $Host.UI.ReadLine()
+    Exit
 }
 
 
