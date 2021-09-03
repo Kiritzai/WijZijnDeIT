32ed4a8779d8cf15e7cffa2e84c868a2294d5b70
Kiritzai <toonen.remy@gmail.com>
2021-09-03 13:00:52 +0200

    Update main.ps1

--- Scripts/Powershell/main.ps1
+++ Scripts/Powershell/main.ps1
@@ -4,8 +4,6 @@
 #
 # Run powershell script by running one of the following 2 commands below
 #
-# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/main.ps1'))
-#
 # Set-ExecutionPolicy Bypass -Scope Process -Force; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::SecurityProtocol -bor 3072; &([scriptblock]::Create((Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -DisableKeepAlive -useb 'https://raw.githubusercontent.com/Kiritzai/WijZijnDeIT/master/Scripts/Powershell/main.ps1')))
 #
 
