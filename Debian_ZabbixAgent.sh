7fc034764e99b4ee641db6d192fc374c87b33a39
Kiritzai <toonen.remy@gmail.com>
2022-03-08 15:38:27 +0100

    Update Debian_ZabbixAgent.sh

--- Debian_ZabbixAgent.sh
+++ Debian_ZabbixAgent.sh
@@ -14,6 +14,15 @@ set +H
 # Software
 SOFTWARE="Zabbix Agent"
 
+RESET='\033[0m'
+YELLOW='\033[1;33m'
+#GRAY='\033[0;37m'
+#WHITE='\033[1;37m'
+GRAY_R='\033[39m'
+WHITE_R='\033[39m'
+RED='\033[1;31m' # Light Red.
+GREEN='\033[1;32m' # Light Green.
+#BOLD='\e[1m'
 
 ##################
 ## Installation ##
