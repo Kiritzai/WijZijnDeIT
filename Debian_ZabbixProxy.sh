39596ab42077e666365c834d635f7a36b03cd7e6
Kiritzai <toonen.remy@gmail.com>
2022-07-07 22:13:01 +0200

    Update Debian_ZabbixProxy.sh

--- Debian_ZabbixProxy.sh
+++ Debian_ZabbixProxy.sh
@@ -87,6 +87,7 @@ main () {
 function installZabbixProxy {
 
 	# Installing Zabbix Repo
+	find / -type f -name "zabbix.db" -delete
 	find / -type f -name "zabbix.list" -delete
 	wget https://repo.zabbix.com/zabbix/6.2/debian/pool/main/z/zabbix-release/zabbix-release_6.2-1+debian$(cut -d"." -f1 /etc/debian_version)_all.deb
 	dpkg -i zabbix-release_6.2-1+debian$(cut -d"." -f1 /etc/debian_version)_all.deb
